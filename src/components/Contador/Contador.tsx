794ca6e79058a12aed8f86fcb77781bc7eecf526
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-16 14:25:43 -0300

    add segundo exemplo

---
+++ src/components/Contador/Contador.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './Contador.css'
+
+function Contador() {
+  //useState = criar uma variável de estado
+  const [numero, setNumero] = useState(0)
+
+  function somarMaisUm(){
+    setNumero(numero + 1)
+  }
+
+  return (
+    <div className="container">
+        <p>O valor é: { numero }</p>
+        <button onClick={somarMaisUm}>Adicionar +1</button>
+    </div>
+  )
+}
+
+export default Contador
